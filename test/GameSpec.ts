2b6b4b4b836c775258be6d24ef8314b924650d30
codelogicws <russ@codelogic.ws>
2016-05-07 14:17:56 -0700

    Add more tests

--- test/GameSpec.ts
+++ test/GameSpec.ts
@@ -26,7 +26,14 @@ describe('Game Tests.', () => {
       it('should know when pieces are in a row', ()=>{
         game.placePiece(0,1,PLAYERS.COMPUTER);
         game.placePiece(2,2,PLAYERS.PLAYER);
-        game.placePiece(1,1,PLAYERS.COMPUTER);
+        game.placePiece(2,1,PLAYERS.COMPUTER);
+        game.test1();
+        expect( game.test1().state ).to.equal( GAMESTATES.Computer1MoveFromWinning);
+      })
+
+      it('should know when pieces are in a diagonal', ()=>{
+        game.placePiece(2,0,PLAYERS.COMPUTER);
+        game.placePiece(0,2,PLAYERS.COMPUTER);
         game.test1();
         expect( game.test1().state ).to.equal( GAMESTATES.Computer1MoveFromWinning);
       })
@@ -41,6 +48,26 @@ describe('Game Tests.', () => {
         expect( game.test1().state ).to.not.equal( GAMESTATES.ComputerWon);
         expect( game.test1().state ).to.not.equal( GAMESTATES.Computer1MoveFromWinning);
       })
+
+      describe('Game should know winning states.', ()=>{
+        it('should know when the user won', ()=>{
+          game.placePiece(1,1,PLAYERS.PLAYER);
+          game.placePiece(1,0,PLAYERS.COMPUTER);
+          game.placePiece(2,2,PLAYERS.PLAYER);
+          game.placePiece(1,2,PLAYERS.COMPUTER);
+          game.placePiece(0,0,PLAYERS.PLAYER);
+          expect( game.test1().state ).to.equal( GAMESTATES.PlayerWon);
+        });
+
+        it('should know when the computer won', ()=>{
+          game.placePiece(2,2,PLAYERS.COMPUTER);
+          game.placePiece(2,1,PLAYERS.PLAYER);
+          game.placePiece(0,2,PLAYERS.COMPUTER);
+          game.placePiece(0,0,PLAYERS.PLAYER);
+          game.placePiece(1,2,PLAYERS.COMPUTER);
+          expect( game.test1().state ).to.equal( GAMESTATES.ComputerWon);
+        });
+      });
     })
 
   })
